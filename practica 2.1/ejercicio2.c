a8daadbd1bed30c326232393f94501caa19527a7
Rocío García <35541675+rocgar08@users.noreply.github.com>
2020-11-26 15:57:48 +0100

    Create ejercicio2.c

---
+++ practica 2.1/ejercicio2.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main (int argc, char **argv){
+	
+	int sid = setuid(0);
+	//setuid sets the  effective  user ID of the calling process
+	if (sid == -1){
+		printf("Error is...%s\n",strerror(sid));
+		perror("setuid");
+		return -1;
+	}
+	printf("No errors \n");
+	return 0;
+}
