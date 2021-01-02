b93acb6f0eed17b987412794170e7e784c868f65
Rooo <rocgar08@ucm.es>
2021-01-02 20:51:19 +0100

    Pequeños cambios

--- practica 2.3/ej11.c
+++ practica 2.3/ej11.c
@@ -6,30 +6,38 @@
 #include <stdlib.h>
 #include <signal.h>
 #include <errno.h>
+void esperandoSen(){
 
+	sigset_t cjto;
+	sigpending(&cjto);
+	if(sigismember(&cjto,SIGINT)==1){
+		printf("Capturando señal SIGINT \n");
+
+	}
+
+	if(sigismember(&cjto,SIGTSTP)==1){
+		printf("Capturando señal SIGTSTP \n");
+
+	}
+
+}
 int main(int argc, char **argv){
 	
 	//Poner en el terminal export SLEEP_SECS="3"
 	sigset_t blk_set;
-
 	sigemptyset(&blk_set);
-
 	sigaddset(&blk_set, SIGINT);
 	sigaddset(&blk_set, SIGTSTP);
-
-	//ACTIVACIÓN DE CERROJO
-	sigprocmask(SIG_BLOCK, &blk_set, NULL);
-	
 
 	char *sleep_sec = getenv("SLEEP_SECS");
 	int s = atoi(sleep_sec);
 	sleep(s);
-	
-	//DESACTIVACIÓN DE CERROJO
+
+	sigprocmask(SIG_BLOCK, &blk_set, NULL);
+
+	esperandoSen();
+
 	sigprocmask(SIG_UNBLOCK, &blk_set, NULL);
-	
-	printf("La señal SIGINT ha sido capturada: %i\n", SIGINT);
-	printf("La señal SIGTSTP ha sido capturada: %i\n", SIGTSTP);
 
 	return 0;
 }
--- practica 2.3/ej8.c
+++ practica 2.3/ej8.c
@@ -5,26 +5,20 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-#include <unistd.h>
 
 
 int main( int argc, char **argv){
 	
-	printf("Estas siendo redirigido al directorio /tmp\n");
+	printf("Estás siendo redirigido al directorio /tmp\n");
 
 	int redir = chdir("/tmp");
-
-	printf("Este es el directorio actual: %s\n",get_current_dir_name());
 	        
 	if(redir == -1){
 		printf("No has sido redirigido al directorio tmp\n");
 		return -1;
 	}
-	
 
 	pid_t pid = fork();
-
-
 	int status,fdOut,fdIn,fdE;
 	
 	
@@ -34,27 +28,30 @@ int main( int argc, char **argv){
 		break;
 		//aprendiz de yedai
 		case 0:
-
-
-				printf("[H] %i [P] %i\n", getpid(),getppid());
-	
-				fdOut = open("/tmp/daemon.out",O_CREAT|O_TRUNC|O_RDWR,0777);
-				fdIn = open("/tmp/daemon.err",O_CREAT|O_TRUNC|O_RDWR,0777);
-				fdE = open("/dev/null",O_CREAT|O_TRUNC,0777);
+				printf("[H] %i [P] %i [(group: %i) (sesion : %i)] \n", getpid(),
+				        getppid(), getpgid(pid), getsid(pid));
+				/*
+				La salida estándar al fichero /tmp/daemon.out.
+				La salida de error estándar al fichero /tmp/daemon.err.
+				La entrada estándar a /dev/null.
+				*/
+				fdOut = open("/tmp/daemon.out",O_CREAT|O_TRUNC,0777);
+				fdIn = open("/dev/null/",O_CREAT|O_TRUNC,0777);
+				fdE = open("tmp/daemon.err",O_CREAT|O_TRUNC,0777);
 				
 				dup2(fdOut,1);
 				dup2(fdIn,0);
 				dup2(fdE,2);
 				execvp(argv[1],argv +1);
-
-
+				
 				
 		break;
 		//yedai
 		default: 
 			wait(status);
-			printf("[P] %i [H] %i \n", getpid(), pid);
-
+			printf("[P] %i [H] %i [(group: %i) (sesion : %i)] \n", 
+				getpid(), pid, getpgid(pid), getsid(pid));
+			execvp(argv[1],argv +1);
 		break;
 	}
 
