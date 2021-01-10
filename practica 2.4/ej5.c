6fb68dbc71959db218268a34b06b2b727054a780
Rooo <rocgar08@ucm.es>
2021-01-10 20:19:35 +0100

    2 tuberías de lectura !! y arreglado el problema de open

--- practica 2.4/ej5.c
+++ practica 2.4/ej5.c
@@ -3,14 +3,12 @@
 #include<stdio.h>
 
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <fcntl.h>
 #include <sys/select.h>
 #include <sys/time.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-
-#define RD 0
-#define WR 1
+#include <errno.h>
 
 int maxi(a,b){ 
 	return a>b?a:b;
@@ -18,67 +16,64 @@ int maxi(a,b){
 
 int main(int argc, char **argv){
 	
+
+	// En el primer terminal ejecuta  ./ej5
+	// En el 2 terminal usas echo > patatas en tubW
 	char buf[256];
+	//2 tuberias lecturas!!!!!!
+	int fdW = open("tubw",O_RDONLY|O_NONBLOCK);
+	int fdR = open("tubr",O_RDONLY|O_NONBLOCK);
 
-	int fdW = open("./tubW",O_RDWR);
-	int fdR = open("./tubR",O_RDWR);
-
-	while(1){
-		printf("Estoy en el bucle \n");
-
+	do{
+		
 		fd_set rfds;
 		FD_ZERO(&rfds);
 		FD_SET(fdW,&rfds);
 		FD_SET(fdR,&rfds);
-		
+			
 		printf("Estoy haciendo el select \n");
-		select(maxi(fdW,fdR)+1,&rfds,NULL,NULL,NULL);
 
-		printf("Ya he hecho el select \n");
-	
+		int sel = select(maxi(fdW,fdR)+1,&rfds,NULL,NULL,NULL);
+		if(sel == -1){
+			printf("Error en sel %s\n",strerror(errno));
+			return -1;
+		}
+
 		int *tubActual;//para el descriptor de fichero de la tuberia actual
 		char *idActual; //Para saber cual es el identificador
 		
-		printf("Estoy haciendo las comprobaciones del FD_ISSET \n");
-		
 		if(FD_ISSET(fdW,&rfds)){
+			printf("tubw \n");
 			tubActual = &fdW;
-			idActual = "tubW";
+			idActual = "tubw";
 		}
 	
-		else if(FD_ISSET(fdR,&rfds)){
-			tubActual =&fdR;
-			idActual = "tubR";
-		}
-		else{
-			printf("Error en el select \n");
-			return -1;
+		if(FD_ISSET(fdR,&rfds)){
+			printf("tubr \n");
+			tubActual = &fdR;
+			idActual = "tubr";
 		}
 		
 		printf("Estoy empezando a leer \n");
-
-		size_t rc = read(*tubActual,buf,255);
+		int rc = read(*tubActual,buf,255);
 
 		if(rc == -1){
 			printf("Hay problemas al realizar la lectura correspondiente de la turbería actual% s\n", buf);
 			return -1;
-
 		}
 		//Indicando el fin de fichero, por lo que hay que cerrar la tubería y volver a abrirla
 		else if(rc == 0){
 			close(*tubActual);
-			*tubActual = open(idActual,O_RDWR); //TAmbién se puede poner el flag RD_ONLY|NON_BLOCK
+			*tubActual = open(idActual,O_RDONLY|O_NONBLOCK); //También se puede poner el flag RD_ONLY|NON_BLOCK
 		}
-
 		else{
-
 			buf[rc] = '\0';
 			printf("Tuberia: %i Datos: %s\n", *idActual,buf);
 		}
-	}
+		
+	}while(1);
 	
 	close(fdW);
 	close(fdR);
-	return 0;
-		
+	return 0;	
 }
