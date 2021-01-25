89043cce6becc830a8ddd6f6054913683610dce4
Rooo <rocgar08@ucm.es>
2021-01-25 09:43:00 +0100

    Simplificación de udphora y multiproceso

--- practica 2.5/ServidorUDPHora2.c
+++ practica 2.5/ServidorUDPHora2.c
@@ -8,15 +8,15 @@
 #include <time.h>
 #define BUF_SIZE 80
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]){
 
-	struct addrinfo hints, *rp;
+	struct addrinfo hints;
+	struct addrinfo *rp;
 	int sfd, s;
 	struct sockaddr_storage peer_addr;
 	socklen_t peer_addr_len;
-	
 	ssize_t nread;
-	char bufS[BUF_SIZE],buf[2],host[NI_MAXHOST], service[NI_MAXSERV];;
+	char buf[BUF_SIZE];
 
 
 	memset(&hints, 0, sizeof(struct addrinfo));
@@ -25,62 +25,48 @@ int main(int argc, char *argv[]) {
 	hints.ai_flags = AI_PASSIVE;    /* For wildcard IP address */
 	hints.ai_protocol = 0;          /* Any protocol */
 
-	// .ej2 192.168.0.1 7777
+
 	s = getaddrinfo(argv[1], argv[2], &hints, &rp);
-
-	if (s == -1) {
+	if (s != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
-		return -1;
+		exit(EXIT_FAILURE);
 	}
-
 	sfd = socket(rp->ai_family, rp->ai_socktype,rp->ai_protocol);
-	
-	if (sfd == -1) {
-		fprintf(stderr, "socket: %s\n", gai_strerror(sfd));
-		return -1;
-	}
-	
-	int b = bind(sfd, rp->ai_addr, rp->ai_addrlen);
-	if (b == -1) {
-		fprintf(stderr, "bind: %s\n", gai_strerror(b));
-		return -1;
-	}
-	
-	freeaddrinfo(rp);      
-	while (1) {
-		//Necesario para sacar el anho y la fecha
-		time_t t = time(NULL);
-		struct tm *tm = localtime(&t);
-		peer_addr_len = sizeof(struct sockaddr_storage);
-		nread = recvfrom(sfd, buf,sizeof(char), 0,(struct sockaddr *) &peer_addr, &peer_addr_len);
-		buf[1]='\0';
-		getnameinfo((struct sockaddr *) &peer_addr,peer_addr_len, host, NI_MAXHOST,service, NI_MAXSERV, NI_NUMERICSERV|NI_NUMERICHOST);
-		printf("Received %ld bytes from %s:%s\n",(long) nread, host, service);
+	bind(sfd, rp->ai_addr, rp->ai_addrlen);
+	freeaddrinfo(rp);           /* No longer needed */
 
+	for (;;) {
+		peer_addr_len = sizeof(struct sockaddr_storage);
+		nread = recvfrom(sfd, buf, BUF_SIZE, 0,
+		(struct sockaddr *) &peer_addr, &peer_addr_len);
+		
+
+		char host[NI_MAXHOST], service[NI_MAXSERV];
+
+		s = getnameinfo((struct sockaddr *) &peer_addr, peer_addr_len, host, NI_MAXHOST,service, NI_MAXSERV, NI_NUMERICSERV|NI_NUMERICHOST);
+
+		printf("Received %ld bytes from %s:%s\n",(long) nread, host, service);
+		time_t t = time(0);
+		struct tm *tm = localtime(&t);
+		size_t fecha;
 		if(buf[0] == 'q'){
 			printf("Bye..\n");
 			return 0;
 		}
-		else if(buf[0] == 't'){
-			size_t anho = strftime(bufS,BUF_SIZE,"%H:%M:%S",tm);
-			bufS[anho] = '\0';
-			sendto(sfd, bufS, anho, 0,(struct sockaddr *) &peer_addr,peer_addr_len);
-			printf("Año: %s\n",bufS);
-
+		if(buf[0] == 'd'){
+			fecha = strftime(buf,79,"%H:%M",tm);
+			buf[fecha] = '\0';
+			sendto(sfd, buf, fecha, 0,(struct sockaddr *) &peer_addr,peer_addr_len);
+			printf("Datos %s\n", buf);
 		}
-		else if(buf[0] == 'd' ){
-			size_t fecha = strftime(bufS,BUF_SIZE,"%Y-%M-%d",tm);
-			bufS[fecha] = '\0';
-			sendto(sfd, bufS, fecha, 0,(struct sockaddr *) &peer_addr,peer_addr_len);
-			printf("Fecha: %s\n",bufS);
-		}
-
-		else{
-			printf(" Error al introducir el comando tiene que ser t,d o q \n");
-			return -1;
-		}
-	}//WHILE(1)
 		
+		if(buf[0] == 't'){
+			fecha = strftime(buf,79,"%d/%m/%y:",tm);
+			buf[fecha] = '\0';
+			sendto(sfd, buf, fecha, 0,(struct sockaddr *) &peer_addr,peer_addr_len);
+			printf("Datos %s\n", buf);
+		}
+	}
 	close(sfd);
 	return 0;
 }
